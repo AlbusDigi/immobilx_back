e15b88c429cfb1ec189ae762f3bc5331860afe98
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-09-05 11:55:30 +0100

    feat: creation de la table logements et mise en relation entre model Logement et Parcelle

---
+++ app/Models/Logement.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Logement extends Model
+{
+    //
+    protected $fillable = [
+        'parcelle_id',         // lien avec la parcelle
+        'name',                // nom du logement
+        'floor',               // étage
+        'rooms',               // nombre de pièces
+        'living_area',         // surface habitable
+        'construction_year',   // année de construction
+        'equipments',          // équipements du logement
+        'rent',                // loyer
+        'charges',             // charges locatives
+        'deposit',             // dépôt de garantie
+        'availability',        // disponible / occupé / maintenance
+        'state',              // actif / bloqué / pending
+        'internal_rules',      // règles propres au logement
+        'note',                // notes propres au logement
+    ];
+
+    public function parcelle()
+    {
+        return $this->belongsTo(Parcelle::class);
+    }
+
+}
