9116ae470577ea31ba21002b4aad1dc9a7d3ba55
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-09-05 09:43:48 +0100

    feat: implementation du crud des parcelles dans ParcelleController

---
+++ app/Http/Resources/Bailleur/ParcelleResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources\Bailleur;
+
+use App\Http\Resources\BailleurResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ParcelleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'bailleur_id' => new BailleurResource($this->bailleur),
+            'name' => $this->name,
+            'address' => $this->address,
+            'area' => $this->area,
+            'internal_rules' => $this->internal_rules,
+            'cadastral_number' => $this->cadastral_number,
+            'land_title_number' => $this->land_title_number,
+            'land_title_date' => $this->land_title_date,
+            'legal_status' => $this->legal_status,
+            'housing_units' => $this->housing_units,
+            'floors' => $this->floors,
+            'construction_year' => $this->construction_year,
+            'urban_zone' => $this->urban_zone,
+            'note' => $this->note,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
